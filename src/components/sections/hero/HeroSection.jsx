a468c34bd537b6f0f33753c743c07397689a653e
boypaida12 <baddysackey5@gmail.com>
2024-07-22 15:05:22 +0000

    WIP: merge loh-updates into main

--- src/components/sections/hero/HeroSection.jsx
+++ src/components/sections/hero/HeroSection.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Heading, Img, Link, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Image, Img, Link, Text } from "@chakra-ui/react";
 import CarouselSlide from "../../shared/CarouselSlide";
 
 const imgSrc = ["/assets/carousel-3.JPG", "/assets/carousel-5.JPG", "/assets/carousel-2.JPG"]
@@ -10,7 +10,7 @@ function HeroSection() {
         <Flex
           fontFamily="fonts.heading"
           alignSelf="center"
-          w={{ lg: "75%" }}
+          w={{ lg: "100%" }}
           flexDir="column"
           gap={4}
         >
@@ -21,23 +21,22 @@ function HeroSection() {
             color="secondary.900"
             textAlign={{ base: "center", lg: "start" }}
           >
-            De-risking Farmland,
+            Smart Farm Management
             <Text as="span" color="primary.900">
               {" "}
-              Empowering Farmers
+              Seamless Market Access
             </Text>
           </Heading>
           <Text
             fontSize={{ base: "md", lg: "2xl" }}
-            w={{ xl: "65%" }}
+            w={{ xl: "75%" }}
             fontWeight="300"
             textAlign={{ base: "center", lg: "start" }}
           >
-            Equipping African Farmers with Technology and Data for
-            Sustainable Agriculture and Increased Crop Yields
+            Equipping African Farmers with Data-Driven Farming, Higher Yields, Traceable Supply Chain 
           </Text>
           <Flex gap={4} flexDir={{ base: "column", lg: "row" }} mx={{base: "auto", lg: "initial"}}>
-            <Link href="#contact">
+            <Link href="https://play.google.com/store/apps/details?id=com.agriguard.mobile">
               <Button
                 color="white"
                 bg="secondary.900"
@@ -46,7 +45,8 @@ function HeroSection() {
                 size="lg"
                 _hover={{ bg: "primary.900" }}
               >
-                Book Farm Mapping
+                <Image src="/assets/playstore.png" w="32px" h="32px" mr={2}/>
+                Download on Playstore
               </Button>
             </Link>
           </Flex>
