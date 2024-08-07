d4a7aeee549633e18ebb693cd2ba59a6d282b580
boypaida12 <baddysackey5@gmail.com>
2024-08-07 01:51:14 +0000

    WIP: cache again

--- app/api/fetchAggregatedFarms/route.ts
+++ app/api/fetchAggregatedFarms/route.ts
@@ -2,18 +2,13 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const response = await fetch('http://178.128.240.96/aggregated-seasons/');
+    const response = await fetch('http://178.128.240.96/aggregated-seasons/', {cache: 'no-store'});
     if (!response.ok) {
       throw new Error('Failed to fetch data');
     }
     const data = await response.json();
     console.log(data)
     return NextResponse.json(data, {
-      headers: {
-        'Cache-Control': 'no-cache, no-store, must-revalidate',
-        'Pragma': 'no-cache',
-        'Expires': '0',
-      },
     });
   } catch (error) {
     return NextResponse.error();
